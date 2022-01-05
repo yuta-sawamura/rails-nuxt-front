82abf0ff1891f6d29e93c1f8ee5d553a19c3ccbe
Yuta Sawamura <>
2022-01-05 09:57:45 +0900

    Add individual project pages

---
+++ layouts/project.vue
@@ -0,0 +1,16 @@
+<template>
+  <v-app>
+    <LoggedInAppBar />
+    <v-main>
+      project.vue
+      <nuxt />
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import LoggedInAppBar from '~/components/LoggedIn/LoggedInAppBar.vue'
+export default {
+  components: { LoggedInAppBar }
+}
+</script>
---
+++ pages/project/_id/components.vue
@@ -0,0 +1,9 @@
+<template>
+  <div>
+    {{ $route.fullPath }}
+  </div>
+</template>
+
+<script>
+export default {}
+</script>
